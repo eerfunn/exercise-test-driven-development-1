d4fa5b1d38fc06e13a9f9c6893eff57b5c60eccb
Irfan Nada Bayu Samudera <irfannadabayusamudera@gmail.com>
2022-06-04 08:01:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is counter app</h1>
+      <h6>This is counter app</h6>
+      <button id="increment-btn" aria-label='button-add' onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button id="decrement-btn" aria-label='button-dec' onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <div aria-label='counter-value'>{counter}</div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Counter Testing', () => {
+  beforeEach(() => {
+    render(<App />)
+
+  })
+  test('renders the title of counter', () => {
+    
+    expect(screen.getByRole('heading', {name:'This is counter app', level: 6})).toHaveTextContent('This is counter app')
+  });
+  test("render a button with text of `Increment`", () => {
+  
+  expect(screen.getByLabelText('button-add').textContent).toBe('Increment')
+  })
+  test("render the initial value of state in a div", () => {
+  expect(screen.getByLabelText('counter-value').textContent).toBe("0")
+  });
+  test('render the click event of increment button and increment counter value ', () => { 
+    const testingWhatever  = async () => {
+      await userEvent.click(screen.getByLabelText('button-add'))
+      expect(screen.getByLabelText('counter-value').textContent).toBe('1')
+    }
+  });
+    test('render the click event of decrement button and increment counter value ', () => { 
+    const testingWhatever  = async () => {
+      await userEvent.click(screen.getByLabelText('button-add'))
+      expect(screen.getByLabelText('counter-value').textContent).toBe('1')
+      await userEvent.click(screen.getByLabelText('button-dec'))
+      expect(screen.getByLabelText('counter-value').textContent).toBe('0')
+    }
+  });
+})
+
+ 
